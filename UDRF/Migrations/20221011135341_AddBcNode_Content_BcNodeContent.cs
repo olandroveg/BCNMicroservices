aec5eb11fab535a05e5edc05830e56e9cf4e9ea9
olandroveg <olandroveg@gmail.com>
2022-10-11 15:54:55 +0200

    1

---
+++ UDRF/Migrations/20221011135341_AddBcNode_Content_BcNodeContent.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UDRF.Migrations
+{
+    public partial class AddBcNode_Content_BcNodeContent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BcNode",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    TopBcNodeId = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci"),
+                    UserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NumberOfSecondBcNodes = table.Column<int>(type: "int", nullable: false),
+                    Ready = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Group = table.Column<int>(type: "int", nullable: false),
+                    BcNodeId = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BcNode", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BcNode_BcNode_BcNodeId",
+                        column: x => x.BcNodeId,
+                        principalTable: "BcNode",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Content",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SourceLocation = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ServicesId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Content", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BcNodeContent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    BcNodeId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    ContentId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Bitrate = table.Column<float>(type: "float", nullable: false),
+                    Size = table.Column<float>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BcNodeContent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BcNodeContent_BcNode_BcNodeId",
+                        column: x => x.BcNodeId,
+                        principalTable: "BcNode",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BcNodeContent_Content_ContentId",
+                        column: x => x.ContentId,
+                        principalTable: "Content",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BcNode_BcNodeId",
+                table: "BcNode",
+                column: "BcNodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BcNodeContent_BcNodeId",
+                table: "BcNodeContent",
+                column: "BcNodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BcNodeContent_ContentId",
+                table: "BcNodeContent",
+                column: "ContentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BcNodeContent");
+
+            migrationBuilder.DropTable(
+                name: "BcNode");
+
+            migrationBuilder.DropTable(
+                name: "Content");
+        }
+    }
+}
