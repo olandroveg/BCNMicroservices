01201f9170dda239d599e411baf18bc48722dea3
olandroveg <olandroveg@gmail.com>
2022-10-26 17:00:25 +0200

    1

---
+++ UDRF/Area/Api/ContentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UDRF.Adapters.ContentAdapter;
+using UDRF.Adapters.ServicesAdapter;
+using UDRF.Services.ContentService;
+using UDRF.Services.ServicesService;
+
+namespace UDRF.Area.Api
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ContentController : Controller
+    {
+        private readonly IContentService _contentService;
+        private readonly IContentAdapter _contentAdapter;
+        private readonly IServicesAdapter _servicesAdapter;
+        private readonly IServicesService _servicesService;
+        
+        public ContentController (IContentService contentService,
+            IContentAdapter contentAdapter,
+            IServicesAdapter servicesAdapter, 
+            IServicesService servicesService)
+            {
+            _contentService = contentService;
+            _contentAdapter = contentAdapter;
+            _servicesAdapter = servicesAdapter;
+            _servicesService = servicesService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetAllContents()
+        {
+            try
+            {
+                var data = _contentAdapter.ConvertContentsToTableDtos(_contentService.GetAllContents());
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
