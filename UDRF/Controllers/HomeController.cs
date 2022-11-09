8e39bc3319d78270579b3ade72a96b8c69a42d30
olandroveg <olandroveg@gmail.com>
2022-11-10 00:11:18 +0100

    1

--- UDRF/Controllers/HomeController.cs
+++ UDRF/Controllers/HomeController.cs
@@ -35,11 +35,13 @@ public class HomeController : Controller
     }
     public async Task< IActionResult> Index()
     {
-        var token = await GetToken();
-        var incomeNF = _nRFService.ConformNFDto();
-        var nfId = await _nRFService.RegisterNF(token, incomeNF);
+        
         try
         {
+            var token = await GetToken();
+            var incomeNF = _nRFService.ConformNFDto();
+            var nfId = await _nRFService.RegisterNF(token, incomeNF);
+        
             await _idNRFService.AddOrUpdate(new IDinNRF
             {
                 Id = nfId
